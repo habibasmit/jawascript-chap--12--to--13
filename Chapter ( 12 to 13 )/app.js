2d23f2faf8888760ffa36a5af1fb95372c6c13ca
Habiba kaleem <153904678+habibasmit@users.noreply.github.com>
2024-05-18 21:59:55 +0500

    chap--12---to---13

---
+++ Chapter ( 12 to 13 )/app.js
@@ -0,0 +1,94 @@
+/*   chapter no 12 to 13
+ if ...else & else if statment , testing set of condition....  */
+//  Question no #01
+// var input = prompt('Enter a character:');
+// if (!isNaN(input)) {
+//     /* Number */
+//     console.log(input + ' is a number.');
+// }else if (input >= 'A' && input <= 'Z') {
+//     /*  Uppercase  */
+//     console.log(input + ' is an uppercase letter.');
+// }else if (input >= 'a' && input <= 'z') {
+//     /* Lowercase */
+//     console.log(input + ' is a lowercase letter.');
+// }else {
+//     console.log(input + ' is not a valid character.');
+// }
+
+
+// Question no #02
+// var num1 = parseInt(prompt('Enter the first integer:'));
+// var num2 = parseInt(prompt('Enter the second integer:'));
+
+// if (num1 === num2) {
+//     console.log("Both numbers are equal.");
+// }else if (num1 > num2) {
+//     console.log(num1 + " is larger than " + num2);
+// } else {
+//     console.log(num2 + " is larger than " + num1);
+// }
+//  Question no #03
+// var num=+prompt('Enter a number to check whether Positive, Negative or Zero');
+// if(num>0){
+//     alert(num+ ') The number is Positive')
+// }
+// else if(num<0){
+//     alert(num+') The number is Negative')
+// }
+// else{
+//     alert(num+') The number is zero')
+// }
+// question no #04
+
+// var character=prompt("Enter a character to check it is a  vowel or Not");
+// if(character==='a'||character==='A'||character==='e'||character==='E'||character==='i'||character==='I'||character==='o'||character==='O'||character==='u'||character==='U'){
+//     document.write(character+ ' is vowel')
+// }
+// else{
+//     document.write(character+' is not vowel')
+// }
+
+
+ // Question no #05
+
+//  var password1 = "habiba123";
+// var userInput = prompt("Enter your password");
+// var password2 = "biba123";
+
+// if (userInput === password1 || userInput >= password2) {
+//   console.log("Correct password");
+// } else {
+//   console.log("Wrong password");
+// } 
+
+
+//  Question no #06
+
+// var greeting = prompt("Enter the day");
+// var hour = 13;
+// if (hour < 18) {
+//   console.log( /*greeting +*/ " Good day");
+// } else {
+//   console.log( /*greeting +*/ " Good evening");
+// }
+
+// Question no #07
+
+// var time = 0;
+// var enterTime = prompt("Enter the time");
+
+// if (enterTime >= 0 && enterTime < 12) {
+//   console.log("Good morning");
+// } else if (enterTime >= 12 && enterTime < 17) {
+//   console.log("Good afternoon");
+// } else if (enterTime >= 17 && enterTime < 21) {
+//   console.log("Good evening");
+// } else if (enterTime >= 21 && enterTime < 24) {
+//   console.log("Good night");
+// } else {
+//   console.log("Enter a valid time");
+// }
+
+
+
+/*    -------------------- chapter  12  to  13  end  -------------------    */
